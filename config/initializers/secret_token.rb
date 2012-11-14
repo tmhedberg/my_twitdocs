b159445e69f99f04849388d9a94a31990cbf04c5
Taylor Hedberg <t@tmh.cc>
2012-11-14 14:25:28 -0500

    `rails new my_twitdocs --skip-active-record

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyTwitdocs::Application.config.secret_token = '26253b353e22643c998c5fb0c8ed7e61eb880430d4ae6f07d7639d2dc9b56fafd46dffb66cccd83f2706a1ebd2e0434b657d3f311c51312a08f1409015a7f7b6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MyTwitdocs::Application.config.session_store :cookie_store, key: '_my_twitdocs_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MyTwitdocs::Application.config.session_store :active_record_store
