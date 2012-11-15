45779c49d7e140e52d701a6717c7b8be4d3b654a
Taylor Hedberg <t@tmh.cc>
2012-11-15 15:42:20 -0500

    Extract document attributes from submitted form parameters
    This is rather ugly; I need to see if there's a better way to accomplish
    it.

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -12,8 +12,14 @@ class DocumentsController < ApplicationController
   def create
     upload = params[:document][:upload_data]
     doc =
-      current_user.documents.create! name: upload.original_filename,
-                                     size: upload.read.size
+      current_user.documents.new name: upload.original_filename,
+                                 size: upload.read.size
+    attrs = {}
+    params[:document].select { |key, _| key =~ /^name-\d+$/ }.each do |key, val|
+      attrs[val] = params[:document]["val-#{key.match(/\d+$/)[0]}"]
+    end
+    doc.attrs = attrs
+    doc.save!
 
     flash[:notice] = 'Document uploaded'
     redirect_to doc
