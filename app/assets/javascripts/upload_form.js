54fbab8bd93bb966bcec449efbf56410a8201cda
Taylor Hedberg <t@tmh.cc>
2012-11-15 15:25:40 -0500

    Dynamically add form fields for document attributes

--- app/assets/javascripts/upload_form.js
+++ app/assets/javascripts/upload_form.js
@@ -1,3 +1,12 @@
 $(function() {
-    alert("foo");
+    var attr_ct = 0;
+    $("#add-attr").click(function() {
+        $("#attrs").append(
+            "<br><input type='text' name='document[name-" +
+                attr_ct +
+                "]'><input type='text' name='document[val-" +
+                attr_ct++ +
+                "]'>"
+        );
+    });
 });
