2fb05dbac1bc4e059682e46432f6c115f8c8a535
Taylor Hedberg <t@tmh.cc>
2012-11-15 12:47:55 -0500

    Document create action

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -6,4 +6,15 @@ class DocumentsController < ApplicationController
   def new
     @doc = Document.new
   end
+
+  def create
+    upload = params[:document][:upload_data]
+    data = upload.read
+    doc =
+      current_user.documents.create! name: upload.original_filename,
+                                     size: data.size
+
+    flash[:notice] = 'Document uploaded'
+    redirect_to doc
+  end
 end
